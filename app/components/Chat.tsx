45ec09f3c9620d8844200a380c28004f825a41f1
Vinh Le <lethanhvinh95@gmail.com>
2025-03-28 07:31:35 +0200

    update env var

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -17,7 +17,7 @@ interface MessageChunk {
   body: string;
 }
 
-const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8080'
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080'
 
 const FinancialChatbox = () => {
   const [messages, setMessages] = useState<Message[]>([]);
