89b834b6a4a455e23304c1698bd4afdcafd2ab1e
Vinh Le <lethanhvinh95@gmail.com>
2025-04-07 20:23:00 +0300

    simpler style

--- app/tickers/[ticker]/KeyStats.tsx
+++ app/tickers/[ticker]/KeyStats.tsx
@@ -14,32 +14,32 @@ export default function KeyStats({keyStats}: {keyStats: KeyStatsType}) {
     <div className="mb-6">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         <div className="stat-card">
-          <h3 className="text-gray-600">Market capitalization</h3>
+          <h3>Market capitalization</h3>
           <p className="text-2xl font-bold">${formatNumber(keyStats.market_cap)}</p>
         </div>
         
         <div className="stat-card">
-          <h3 className="text-gray-600">Price to earnings Ratio (TTM)</h3>
+          <h3>Price to earnings Ratio (TTM)</h3>
           <p className="text-2xl font-bold">{keyStats.pe_ratio}</p>
         </div>
         
         <div className="stat-card">
-          <h3 className="text-gray-600">Dividend yield (indicated)</h3>
+          <h3>Dividend yield (indicated)</h3>
           <p className="text-2xl font-bold">{keyStats.dividend_yield}%</p>
         </div>
         
         <div className="stat-card">
-          <h3 className="text-gray-600">Basic EPS (TTM)</h3>
+          <h3>Basic EPS (TTM)</h3>
           <p className="text-2xl font-bold">${keyStats.basic_eps}</p>
         </div>
         
         <div className="stat-card">
-          <h3 className="text-gray-600">Net income (FY)</h3>
+          <h3>Net income (FY)</h3>
           <p className="text-2xl font-bold">${formatNumber(keyStats.net_income)}</p>
         </div>
         
         <div className="stat-card">
-          <h3 className="text-gray-600">Revenue (FY)</h3>
+          <h3>Revenue (FY)</h3>
           <p className="text-2xl font-bold">${formatNumber(keyStats.revenue)}</p>
         </div>
       </div>
