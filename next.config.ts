a276e4d41484913d53f3e7c77abb6de21b0ad845
Vinh Le <lethanhvinh95@gmail.com>
2025-06-08 08:27:10 +0300

    add comment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  // Only configure caching on root route here. Caching for individual routes are configured by "revalidate" constant in their corresponding pages
   async headers() {
     return [
       {
